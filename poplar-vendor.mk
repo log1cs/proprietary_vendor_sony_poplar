e239cbe81f3a29c7c8ecfa108916170d1f36715b
Tuan Anh <tuan73176@gmail.com>
2026-05-01 17:31:39 +0000

    G8342-user 9 47.2.A.11.228 3311891731 release-keys
    Change-Id: I9a299d2b1e0effdea87e6223d4f5f091f4475016

---
+++ poplar-vendor.mk
@@ -0,0 +1,104 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+PRODUCT_SOONG_NAMESPACES += \
+    vendor/sony/poplar
+
+PRODUCT_COPY_FILES += \
+    vendor/sony/poplar/proprietary/recovery/root/vendor/firmware/touch_module_id_0xe0.img:$(TARGET_COPY_OUT_RECOVERY)/root/vendor/firmware/touch_module_id_0xe0.img \
+    vendor/sony/poplar/proprietary/recovery/root/vendor/firmware/touch_module_id_0xe1.img:$(TARGET_COPY_OUT_RECOVERY)/root/vendor/firmware/touch_module_id_0xe1.img \
+    vendor/sony/poplar/proprietary/vendor/camera/SEM13BS1/color_ctrl.dat:$(TARGET_COPY_OUT_VENDOR)/camera/SEM13BS1/color_ctrl.dat \
+    vendor/sony/poplar/proprietary/vendor/camera/SEM13BS1/dataflow.dat:$(TARGET_COPY_OUT_VENDOR)/camera/SEM13BS1/dataflow.dat \
+    vendor/sony/poplar/proprietary/vendor/camera/SEM13BS1/exposure_ctrl.dat:$(TARGET_COPY_OUT_VENDOR)/camera/SEM13BS1/exposure_ctrl.dat \
+    vendor/sony/poplar/proprietary/vendor/camera/SEM13BS1/exposure_ctrl_diagram.dat:$(TARGET_COPY_OUT_VENDOR)/camera/SEM13BS1/exposure_ctrl_diagram.dat \
+    vendor/sony/poplar/proprietary/vendor/camera/SEM13BS1/exposure_ctrl_gamma.dat:$(TARGET_COPY_OUT_VENDOR)/camera/SEM13BS1/exposure_ctrl_gamma.dat \
+    vendor/sony/poplar/proprietary/vendor/camera/SEM13BS1/face_detector.dat:$(TARGET_COPY_OUT_VENDOR)/camera/SEM13BS1/face_detector.dat \
+    vendor/sony/poplar/proprietary/vendor/camera/SEM13BS1/flicker_detector.dat:$(TARGET_COPY_OUT_VENDOR)/camera/SEM13BS1/flicker_detector.dat \
+    vendor/sony/poplar/proprietary/vendor/camera/SEM13BS1/focus_stats_conv.dat:$(TARGET_COPY_OUT_VENDOR)/camera/SEM13BS1/focus_stats_conv.dat \
+    vendor/sony/poplar/proprietary/vendor/camera/SEM13BS1/food_comp_yummy.dat:$(TARGET_COPY_OUT_VENDOR)/camera/SEM13BS1/food_comp_yummy.dat \
+    vendor/sony/poplar/proprietary/vendor/camera/SEM13BS1/food_detector.dat:$(TARGET_COPY_OUT_VENDOR)/camera/SEM13BS1/food_detector.dat \
+    vendor/sony/poplar/proprietary/vendor/camera/SEM13BS1/hal_ctrl.dat:$(TARGET_COPY_OUT_VENDOR)/camera/SEM13BS1/hal_ctrl.dat \
+    vendor/sony/poplar/proprietary/vendor/camera/SEM13BS1/hal_ctrl_platform_abf.dat:$(TARGET_COPY_OUT_VENDOR)/camera/SEM13BS1/hal_ctrl_platform_abf.dat \
+    vendor/sony/poplar/proprietary/vendor/camera/SEM13BS1/hal_ctrl_platform_asf_lin.dat:$(TARGET_COPY_OUT_VENDOR)/camera/SEM13BS1/hal_ctrl_platform_asf_lin.dat \
+    vendor/sony/poplar/proprietary/vendor/camera/SEM13BS1/hal_ctrl_platform_asf_stp.dat:$(TARGET_COPY_OUT_VENDOR)/camera/SEM13BS1/hal_ctrl_platform_asf_stp.dat \
+    vendor/sony/poplar/proprietary/vendor/camera/SEM13BS1/hal_ctrl_platform_eccc.dat:$(TARGET_COPY_OUT_VENDOR)/camera/SEM13BS1/hal_ctrl_platform_eccc.dat \
+    vendor/sony/poplar/proprietary/vendor/camera/SEM13BS1/hal_ctrl_platform_gic.dat:$(TARGET_COPY_OUT_VENDOR)/camera/SEM13BS1/hal_ctrl_platform_gic.dat \
+    vendor/sony/poplar/proprietary/vendor/camera/SEM13BS1/hal_ctrl_platform_ltm.dat:$(TARGET_COPY_OUT_VENDOR)/camera/SEM13BS1/hal_ctrl_platform_ltm.dat \
+    vendor/sony/poplar/proprietary/vendor/camera/SEM13BS1/hal_ctrl_platform_nree.dat:$(TARGET_COPY_OUT_VENDOR)/camera/SEM13BS1/hal_ctrl_platform_nree.dat \
+    vendor/sony/poplar/proprietary/vendor/camera/SEM13BS1/hdr_comp.dat:$(TARGET_COPY_OUT_VENDOR)/camera/SEM13BS1/hdr_comp.dat \
+    vendor/sony/poplar/proprietary/vendor/camera/SEM13BS1/idt_ctrl.dat:$(TARGET_COPY_OUT_VENDOR)/camera/SEM13BS1/idt_ctrl.dat \
+    vendor/sony/poplar/proprietary/vendor/camera/SEM13BS1/image_conv.dat:$(TARGET_COPY_OUT_VENDOR)/camera/SEM13BS1/image_conv.dat \
+    vendor/sony/poplar/proprietary/vendor/camera/SEM13BS1/image_conv_canele.dat:$(TARGET_COPY_OUT_VENDOR)/camera/SEM13BS1/image_conv_canele.dat \
+    vendor/sony/poplar/proprietary/vendor/camera/SEM13BS1/image_conv_cheesescone.dat:$(TARGET_COPY_OUT_VENDOR)/camera/SEM13BS1/image_conv_cheesescone.dat \
+    vendor/sony/poplar/proprietary/vendor/camera/SEM13BS1/image_conv_dars.dat:$(TARGET_COPY_OUT_VENDOR)/camera/SEM13BS1/image_conv_dars.dat \
+    vendor/sony/poplar/proprietary/vendor/camera/SEM13BS1/image_conv_marble.dat:$(TARGET_COPY_OUT_VENDOR)/camera/SEM13BS1/image_conv_marble.dat \
+    vendor/sony/poplar/proprietary/vendor/camera/SEM13BS1/image_conv_raisin.dat:$(TARGET_COPY_OUT_VENDOR)/camera/SEM13BS1/image_conv_raisin.dat \
+    vendor/sony/poplar/proprietary/vendor/camera/SEM13BS1/image_conv_spica.dat:$(TARGET_COPY_OUT_VENDOR)/camera/SEM13BS1/image_conv_spica.dat \
+    vendor/sony/poplar/proprietary/vendor/camera/SEM13BS1/lens_ctrl.dat:$(TARGET_COPY_OUT_VENDOR)/camera/SEM13BS1/lens_ctrl.dat \
+    vendor/sony/poplar/proprietary/vendor/camera/SEM13BS1/motion_detector.dat:$(TARGET_COPY_OUT_VENDOR)/camera/SEM13BS1/motion_detector.dat \
+    vendor/sony/poplar/proprietary/vendor/camera/SEM13BS1/motion_detector_sumomo.dat:$(TARGET_COPY_OUT_VENDOR)/camera/SEM13BS1/motion_detector_sumomo.dat \
+    vendor/sony/poplar/proprietary/vendor/camera/SEM13BS1/motion_estimation.dat:$(TARGET_COPY_OUT_VENDOR)/camera/SEM13BS1/motion_estimation.dat \
+    vendor/sony/poplar/proprietary/vendor/camera/SEM13BS1/multiframe_nr.dat:$(TARGET_COPY_OUT_VENDOR)/camera/SEM13BS1/multiframe_nr.dat \
+    vendor/sony/poplar/proprietary/vendor/camera/SEM13BS1/multiframe_nr_melonpan.dat:$(TARGET_COPY_OUT_VENDOR)/camera/SEM13BS1/multiframe_nr_melonpan.dat \
+    vendor/sony/poplar/proprietary/vendor/camera/SEM13BS1/multiframe_nr_mugichoco.dat:$(TARGET_COPY_OUT_VENDOR)/camera/SEM13BS1/multiframe_nr_mugichoco.dat \
+    vendor/sony/poplar/proprietary/vendor/camera/SEM13BS1/multiframe_nr_toppo.dat:$(TARGET_COPY_OUT_VENDOR)/camera/SEM13BS1/multiframe_nr_toppo.dat \
+    vendor/sony/poplar/proprietary/vendor/camera/SEM13BS1/object_tracker.dat:$(TARGET_COPY_OUT_VENDOR)/camera/SEM13BS1/object_tracker.dat \
+    vendor/sony/poplar/proprietary/vendor/camera/SEM13BS1/prc_image_conv.dat:$(TARGET_COPY_OUT_VENDOR)/camera/SEM13BS1/prc_image_conv.dat \
+    vendor/sony/poplar/proprietary/vendor/camera/SEM13BS1/scene_detector.dat:$(TARGET_COPY_OUT_VENDOR)/camera/SEM13BS1/scene_detector.dat \
+    vendor/sony/poplar/proprietary/vendor/camera/SOI13BS1/color_ctrl.dat:$(TARGET_COPY_OUT_VENDOR)/camera/SOI13BS1/color_ctrl.dat \
+    vendor/sony/poplar/proprietary/vendor/camera/SOI13BS1/dataflow.dat:$(TARGET_COPY_OUT_VENDOR)/camera/SOI13BS1/dataflow.dat \
+    vendor/sony/poplar/proprietary/vendor/camera/SOI13BS1/exposure_ctrl.dat:$(TARGET_COPY_OUT_VENDOR)/camera/SOI13BS1/exposure_ctrl.dat \
+    vendor/sony/poplar/proprietary/vendor/camera/SOI13BS1/exposure_ctrl_diagram.dat:$(TARGET_COPY_OUT_VENDOR)/camera/SOI13BS1/exposure_ctrl_diagram.dat \
+    vendor/sony/poplar/proprietary/vendor/camera/SOI13BS1/exposure_ctrl_gamma.dat:$(TARGET_COPY_OUT_VENDOR)/camera/SOI13BS1/exposure_ctrl_gamma.dat \
+    vendor/sony/poplar/proprietary/vendor/camera/SOI13BS1/face_detector.dat:$(TARGET_COPY_OUT_VENDOR)/camera/SOI13BS1/face_detector.dat \
+    vendor/sony/poplar/proprietary/vendor/camera/SOI13BS1/flicker_detector.dat:$(TARGET_COPY_OUT_VENDOR)/camera/SOI13BS1/flicker_detector.dat \
+    vendor/sony/poplar/proprietary/vendor/camera/SOI13BS1/focus_stats_conv.dat:$(TARGET_COPY_OUT_VENDOR)/camera/SOI13BS1/focus_stats_conv.dat \
+    vendor/sony/poplar/proprietary/vendor/camera/SOI13BS1/food_comp_yummy.dat:$(TARGET_COPY_OUT_VENDOR)/camera/SOI13BS1/food_comp_yummy.dat \
+    vendor/sony/poplar/proprietary/vendor/camera/SOI13BS1/food_detector.dat:$(TARGET_COPY_OUT_VENDOR)/camera/SOI13BS1/food_detector.dat \
+    vendor/sony/poplar/proprietary/vendor/camera/SOI13BS1/hal_ctrl.dat:$(TARGET_COPY_OUT_VENDOR)/camera/SOI13BS1/hal_ctrl.dat \
+    vendor/sony/poplar/proprietary/vendor/camera/SOI13BS1/hal_ctrl_platform_abf.dat:$(TARGET_COPY_OUT_VENDOR)/camera/SOI13BS1/hal_ctrl_platform_abf.dat \
+    vendor/sony/poplar/proprietary/vendor/camera/SOI13BS1/hal_ctrl_platform_asf_lin.dat:$(TARGET_COPY_OUT_VENDOR)/camera/SOI13BS1/hal_ctrl_platform_asf_lin.dat \
+    vendor/sony/poplar/proprietary/vendor/camera/SOI13BS1/hal_ctrl_platform_asf_stp.dat:$(TARGET_COPY_OUT_VENDOR)/camera/SOI13BS1/hal_ctrl_platform_asf_stp.dat \
+    vendor/sony/poplar/proprietary/vendor/camera/SOI13BS1/hal_ctrl_platform_eccc.dat:$(TARGET_COPY_OUT_VENDOR)/camera/SOI13BS1/hal_ctrl_platform_eccc.dat \
+    vendor/sony/poplar/proprietary/vendor/camera/SOI13BS1/hal_ctrl_platform_gic.dat:$(TARGET_COPY_OUT_VENDOR)/camera/SOI13BS1/hal_ctrl_platform_gic.dat \
+    vendor/sony/poplar/proprietary/vendor/camera/SOI13BS1/hal_ctrl_platform_ltm.dat:$(TARGET_COPY_OUT_VENDOR)/camera/SOI13BS1/hal_ctrl_platform_ltm.dat \
+    vendor/sony/poplar/proprietary/vendor/camera/SOI13BS1/hal_ctrl_platform_nree.dat:$(TARGET_COPY_OUT_VENDOR)/camera/SOI13BS1/hal_ctrl_platform_nree.dat \
+    vendor/sony/poplar/proprietary/vendor/camera/SOI13BS1/hdr_comp.dat:$(TARGET_COPY_OUT_VENDOR)/camera/SOI13BS1/hdr_comp.dat \
+    vendor/sony/poplar/proprietary/vendor/camera/SOI13BS1/idt_ctrl.dat:$(TARGET_COPY_OUT_VENDOR)/camera/SOI13BS1/idt_ctrl.dat \
+    vendor/sony/poplar/proprietary/vendor/camera/SOI13BS1/image_conv.dat:$(TARGET_COPY_OUT_VENDOR)/camera/SOI13BS1/image_conv.dat \
+    vendor/sony/poplar/proprietary/vendor/camera/SOI13BS1/image_conv_canele.dat:$(TARGET_COPY_OUT_VENDOR)/camera/SOI13BS1/image_conv_canele.dat \
+    vendor/sony/poplar/proprietary/vendor/camera/SOI13BS1/image_conv_cheesescone.dat:$(TARGET_COPY_OUT_VENDOR)/camera/SOI13BS1/image_conv_cheesescone.dat \
+    vendor/sony/poplar/proprietary/vendor/camera/SOI13BS1/image_conv_dars.dat:$(TARGET_COPY_OUT_VENDOR)/camera/SOI13BS1/image_conv_dars.dat \
+    vendor/sony/poplar/proprietary/vendor/camera/SOI13BS1/image_conv_marble.dat:$(TARGET_COPY_OUT_VENDOR)/camera/SOI13BS1/image_conv_marble.dat \
+    vendor/sony/poplar/proprietary/vendor/camera/SOI13BS1/image_conv_raisin.dat:$(TARGET_COPY_OUT_VENDOR)/camera/SOI13BS1/image_conv_raisin.dat \
+    vendor/sony/poplar/proprietary/vendor/camera/SOI13BS1/image_conv_spica.dat:$(TARGET_COPY_OUT_VENDOR)/camera/SOI13BS1/image_conv_spica.dat \
+    vendor/sony/poplar/proprietary/vendor/camera/SOI13BS1/lens_ctrl.dat:$(TARGET_COPY_OUT_VENDOR)/camera/SOI13BS1/lens_ctrl.dat \
+    vendor/sony/poplar/proprietary/vendor/camera/SOI13BS1/motion_detector.dat:$(TARGET_COPY_OUT_VENDOR)/camera/SOI13BS1/motion_detector.dat \
+    vendor/sony/poplar/proprietary/vendor/camera/SOI13BS1/motion_detector_sumomo.dat:$(TARGET_COPY_OUT_VENDOR)/camera/SOI13BS1/motion_detector_sumomo.dat \
+    vendor/sony/poplar/proprietary/vendor/camera/SOI13BS1/motion_estimation.dat:$(TARGET_COPY_OUT_VENDOR)/camera/SOI13BS1/motion_estimation.dat \
+    vendor/sony/poplar/proprietary/vendor/camera/SOI13BS1/multiframe_nr.dat:$(TARGET_COPY_OUT_VENDOR)/camera/SOI13BS1/multiframe_nr.dat \
+    vendor/sony/poplar/proprietary/vendor/camera/SOI13BS1/multiframe_nr_melonpan.dat:$(TARGET_COPY_OUT_VENDOR)/camera/SOI13BS1/multiframe_nr_melonpan.dat \
+    vendor/sony/poplar/proprietary/vendor/camera/SOI13BS1/multiframe_nr_mugichoco.dat:$(TARGET_COPY_OUT_VENDOR)/camera/SOI13BS1/multiframe_nr_mugichoco.dat \
+    vendor/sony/poplar/proprietary/vendor/camera/SOI13BS1/multiframe_nr_toppo.dat:$(TARGET_COPY_OUT_VENDOR)/camera/SOI13BS1/multiframe_nr_toppo.dat \
+    vendor/sony/poplar/proprietary/vendor/camera/SOI13BS1/object_tracker.dat:$(TARGET_COPY_OUT_VENDOR)/camera/SOI13BS1/object_tracker.dat \
+    vendor/sony/poplar/proprietary/vendor/camera/SOI13BS1/prc_image_conv.dat:$(TARGET_COPY_OUT_VENDOR)/camera/SOI13BS1/prc_image_conv.dat \
+    vendor/sony/poplar/proprietary/vendor/camera/SOI13BS1/scene_detector.dat:$(TARGET_COPY_OUT_VENDOR)/camera/SOI13BS1/scene_detector.dat \
+    vendor/sony/poplar/proprietary/vendor/etc/ad_calib.cfg:$(TARGET_COPY_OUT_VENDOR)/etc/ad_calib.cfg \
+    vendor/sony/poplar/proprietary/vendor/etc/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
+    vendor/sony/poplar/proprietary/vendor/etc/qdcm_calib_data_6.xml:$(TARGET_COPY_OUT_VENDOR)/etc/qdcm_calib_data_6.xml \
+    vendor/sony/poplar/proprietary/vendor/etc/qdcm_calib_data_9.xml:$(TARGET_COPY_OUT_VENDOR)/etc/qdcm_calib_data_9.xml \
+    vendor/sony/poplar/proprietary/vendor/etc/sensors/sensor_def_somc_product.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/sensor_def_somc_product.conf \
+    vendor/sony/poplar/proprietary/vendor/etc/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf \
+    vendor/sony/poplar/proprietary/vendor/etc/touch/images/big_touch.png:$(TARGET_COPY_OUT_VENDOR)/etc/touch/images/big_touch.png \
+    vendor/sony/poplar/proprietary/vendor/etc/touch/images/splash.png:$(TARGET_COPY_OUT_VENDOR)/etc/touch/images/splash.png \
+    vendor/sony/poplar/proprietary/vendor/etc/wifi/bdwlan.bin:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/bdwlan.bin \
+    vendor/sony/poplar/proprietary/vendor/firmware/touch_module_id_0xe0.img:$(TARGET_COPY_OUT_VENDOR)/firmware/touch_module_id_0xe0.img \
+    vendor/sony/poplar/proprietary/vendor/firmware/touch_module_id_0xe1.img:$(TARGET_COPY_OUT_VENDOR)/firmware/touch_module_id_0xe1.img \
+    vendor/sony/poplar/proprietary/vendor/firmware/tzfingerprint.b01:$(TARGET_COPY_OUT_VENDOR)/firmware/tzfingerprint.b01 \
+    vendor/sony/poplar/proprietary/vendor/firmware/tzfingerprint.mdt:$(TARGET_COPY_OUT_VENDOR)/firmware/tzfingerprint.mdt \
+    vendor/sony/poplar/proprietary/vendor/usr/idc/clearpad.idc:$(TARGET_COPY_OUT_VENDOR)/usr/idc/clearpad.idc
+
+PRODUCT_PACKAGES += \
+    lib_fpc_tac_shared \
+    fpc_fingerprint@2.1_HIDL-service
